14487483d1afbfa1c45f01a7a193f64a969fa5eb
Mason Erwine <merwine33@gmail.com>
2024-04-11 11:04:16 -0400

    Updated repos and added pico utils

--- scripts/install_dev/install_dev.bash
+++ scripts/install_dev/install_dev.bash
@@ -26,6 +26,9 @@ rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -y -r
 source /opt/ros/$ROS_DISTRO/setup.bash
 colcon build
 
+# pico utils
+sudo apt install -y cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential
+
 # build development software (this should pass)
 echo "Building riptide software"
 cd ~/osu-uwrt/development/software
